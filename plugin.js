1d582e7d8865afef05c4efa2ce30a81c07c09245
xdSi <3450236968@qq.com>
2021-05-28 00:12:54 +0800

    feat: jsx -> js

--- plugin.js
+++ plugin.js
@@ -1,9 +1,8 @@
-import { dataToEsm } from '@rollup/pluginutils'
+import { dataToEsm } from '@rollup/pluginutils';
 const cssLangs = `\\.(scss|styl|stylus|pcss|postcss)($|\\?)`;
 const cssLangRE = new RegExp(cssLangs);
 const cssModuleRE = new RegExp(`\\.module${cssLangs}`);
-const modulesOptions = { scopeBehaviour: 'local', localsConvention: 'camelCaseOnly' }
-
+const modulesOptions = { scopeBehaviour: 'local', localsConvention: 'camelCaseOnly' };
 
 async function compileCSS(id, code) {
   let modules;
@@ -11,40 +10,32 @@ async function compileCSS(id, code) {
   postcssPlugins.unshift(
     (await import('postcss-modules')).default({
       ...modulesOptions,
-      getJSON(
-        cssFileName,
-        _modules,
-        outputFileName
-      ) {
-        modules = _modules
+      getJSON(cssFileName, _modules, outputFileName) {
+        modules = _modules;
         if (modulesOptions && typeof modulesOptions.getJSON === 'function') {
-          modulesOptions.getJSON(cssFileName, _modules, outputFileName)
+          modulesOptions.getJSON(cssFileName, _modules, outputFileName);
         }
       }
     })
-  )
+  );
 
-  const postcssResult = await (await import('postcss'))
-  .default(postcssPlugins)
-  .process(code, {
+  const postcssResult = await (await import('postcss')).default(postcssPlugins).process(code, {
     to: id,
     from: id,
     map: {
       inline: false,
-      annotation: false,
+      annotation: false
     }
-  })
+  });
 
   return {
     ast: postcssResult,
     modules,
-    code: postcssResult.css,
-  }
+    code: postcssResult.css
+  };
 }
 
-
 export let exportModules;
-export let exportCss;
 
 export default function viteTransformCSSModulesPlugin() {
   const name = 'vite-plugin-transform-css-modules';
@@ -53,21 +44,19 @@ export default function viteTransformCSSModulesPlugin() {
     name,
     async transform(raw, id) {
       if (cssLangRE.test(id) && !id.includes('node_modules') && !cssModuleRE.test(id)) {
-        const {
-          code: css,
-          modules
-        } = await compileCSS(id, raw)
-      
+        // 获取模块化后的css源代码 以及 模块化的对象名
+        const { code: css, modules } = await compileCSS(id, raw);
+        // 使用dataToEsm将模块化对象转化为字符串
         const modulesCode =
-        modules && dataToEsm(modules, { namedExports: true, preferConst: true })
+          modules && dataToEsm(modules, { namedExports: true, preferConst: true });
+        // 导出模块话后的字符串给后置的插件使用
         exportModules = modulesCode;
-        exportCss = css;
         return {
           code: css,
           map: { mappings: '' }
-        }
+        };
       }
       return undefined;
-    },
+    }
   };
 }
--- src/page/User.js
+++ src/page/User.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import style from './style1.scss';
 const User = () => {
+  console.log('style',style);
   return (
-    <div>
+    <div className={style.container}>
       User
       <i className="iconfont icontubiaobeifen5" />
     </div>
